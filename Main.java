bb8952e51380ac10884130e525049f7aeaf7d7e7
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-04-21 00:36:17 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
+/*
+Name: Sehajdeep Singh sikka
+PRN: 23070126119
+Batch: AIML B2
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of ThreadManager to manage and run threads
+        ThreadManager manager = new ThreadManager();
+
+        // Call the method to demonstrate execution of both Thread and Runnable types
+        manager.runAllThreads();
+    }
+}
 
